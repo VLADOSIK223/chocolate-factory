a5c52095746c75811d4216544dce977dafc9dead
VLADOSIK223 <mokki.51.mokki@gmail.com>
2026-05-26 17:45:22 +0300

    Обновлен импорт родительского класса в дочернем проекте

--- src/ChocolateBar.php
+++ src/ChocolateBar.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Core\Snack; 
+
 class ChocolateBar extends Snack
 {
-    /**
-     * Специфический метод нарезки для батончика.
-     */
     public function cut(): void
     {
         echo "snack нужно резать по вертикали(кусочками, кубиками, плитками и т.д.)" . PHP_EOL;
